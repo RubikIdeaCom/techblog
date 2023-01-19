cb7a1e3227d7b07a215c9d39fb101eef7ddefbca
Ali Seyedabadi <rubikideacom@gmail.com>
2023-01-19 18:32:36 +0330

    write article/podcast bottom sheet

--- lib/controller/register_controller.dart
+++ lib/controller/register_controller.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:tec/component/api_constants.dart';
@@ -73,7 +75,84 @@ class RegisterController extends GetxController {
       Get.to(() => RegisterIntro());
     } else {
       // User verified
-      debugPrint('Post Screen');
+      routeToWriteBottomSheet();
     }
+  }
+
+  routeToWriteBottomSheet() {
+    Container(
+      height: Get.height / 3,
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                SvgPicture.asset(
+                  "assets/images/techbot.svg",
+                  height: 40,
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                const Text('دانسته هات رو با بقیه به اشتراک بگذار')
+              ],
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Text(
+                'توی دنیای گیگ ها بیا و دانسته هات رو با بقیه به اشتراک بگذار'),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    debugPrint('write article');
+                  },
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          "assets/icons/write_article.png",
+                          height: 32,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        const Text('مدیریت مقاله ها')
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    debugPrint('write podcast');
+                  },
+                  child: Container(
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          "assets/icons/write_podcast.png",
+                          height: 32,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        const Text('مدیریت پادکست ها')
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
